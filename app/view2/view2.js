d6e2373a3f88560cfc87fbb07c2ee9927d05bace
Abhishek <abhishek.garg1191@gmail.com>
2016-12-11 18:39:30 +0530

    initial commit

---
+++ app/view2/view2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myApp.view2', ['ngRoute'])
+
+// .config(['$routeProvider', function($routeProvider) {
+//
+// }])
+
+.controller('View2Ctrl', [function() {
+
+}]);
